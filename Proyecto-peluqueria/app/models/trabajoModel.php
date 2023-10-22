aee6a863d7f562470a5c16ead627f5f1e13ceacd
sntxDev <sntxdev@gmail.com>
2023-10-22 05:21:46 -0300

    corrección de comentarios

--- Proyecto-peluqueria/app/models/trabajoModel.php
+++ Proyecto-peluqueria/app/models/trabajoModel.php
@@ -6,11 +6,9 @@ class TrabajoModel extends Model
 
     public function getTrabajos()
     {
-        // $stmt = $this->db->prepare('SELECT trabajos.*, Clientes.nombre AS nombreCliente FROM Trabajos JOIN Clientes ON Trabajos.cliente_id = Clientes.cliente_id WHERE Trabajos.cliente_id = Clientes.cliente_id');
         $stmt = $this->db->prepare('SELECT DISTINCT c.nombre AS NombreCliente, t.fecha_trabajo, t.trabajo_realizado, t.descripcion, t.cliente_id
         FROM Trabajos t
         JOIN Clientes c ON t.cliente_id = c.cliente_id');
-        // $stmt = $this->db->prepare('SELECT * FROM trabajos');
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
